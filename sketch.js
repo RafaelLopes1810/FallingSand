b1c39cd1e5276e6b150f2dd80e5bd29e486f9d76
Rafael Lopes <rafaeldeoliveiracl@gmail.com>
2025-07-16 19:41:47 -0300

    Add areia caindo funcionando

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ function criaArray2D(coluna, linha){
     let array = new Array(coluna);
     for (let i = 0; i < array.length; i++) {
         array[i] = new Array(linha); 
+        for(let j = 0; j < array[i].length; j++){
+            array[i][j] = 0;
+        }
     }
     return array;
 }
@@ -11,7 +14,7 @@ let tam = 10;
 let coluna, linha;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
     coluna = width / tam;
     linha = height / tam;
     grid = criaArray2D(coluna, linha);
@@ -23,16 +26,67 @@ function setup() {
     }
 }
 
-function draw(){
-    background(0);
+function mouseDragged(){
+    let mouseCol = floor(mouseX / tam);
+    let mouseLin = floor(mouseY / tam);
 
-    for(let i = 0; i < coluna; i++){
-        for(let j = 0; j < linha; j++){
-            stroke(255);
-            fill(grid[i][j] * 255);
-            let x = i * tam;
-            let y = j * tam;
-            square(x, y, tam);
+    let tamanho = 3;
+    let extencao = floor(tamanho/2);
+    for(let i = -extencao; i <= extencao; i++){
+        for(let j = -extencao; j <= extencao; j++){
+            if(random(1) < 0.75){
+                let col = mouseCol + i;
+                let lin = mouseLin + j;
+                if(col >= 0 && col <= coluna - 1 && lin >= 0 && lin <= linha-1){ // Verifica se o mouse está dentro do Canvas
+                    grid[col][lin] = 1;
+                }
+            }
         }
     }
+
+}
+
+function draw(){
+    background(0);
+    for(let i = 0; i < coluna; i++){
+        for(let j = 0; j < linha; j++){
+            noStroke();
+            if(grid[i][j] == 1){
+                fill(grid[i][j] * 255);
+                let x = i * tam;
+                let y = j * tam;
+                square(x, y, tam);
+            }
+        }
+    }
+    
+    let proxGrid = criaArray2D(coluna, linha);
+    for(let i = 0; i < coluna; i++){
+            for(let j = 0; j < linha; j++){
+                let valor = grid[i][j];
+                if(valor === 1){
+                    let embaixo = grid[i][j+1];
+
+                    let dir = random([-1, 1]); // Número entre -1 e 1 para a areia ir aleatoriamente pra direita ou esquerda caso já tenha areia embaixo
+                    let baixoA, baixoB;
+                    if(i + dir >= 0 && i + dir <= coluna - 1){
+                        baixoA = grid[i+dir][j+1];
+                    }
+                    if(i - dir >= 0 && i - dir <= coluna - 1){
+                        baixoB = grid[i-dir][j+1];
+                    }
+
+                    if(embaixo === 0){
+                        proxGrid[i][j+1] = 1;
+                    } else if(baixoA === 0){
+                        proxGrid[i+dir][j+1] = 1;
+                    } else if(baixoB === 0){
+                        proxGrid[i-dir][j+1] = 1;
+                    } else {
+                        proxGrid[i][j] = 1;
+                    }
+                }
+            }
+        }
+    grid = proxGrid;
 }
